c2c8935f605e33acde2c7dd83587c9ea73495320
Luis Ernesto Hernandez Rivera <contacto@lernesto.com>
2023-10-20 00:10:44 -0600

    se agrego la navegación y animaciones

--- src/script.js
+++ src/script.js
@@ -1,20 +1,42 @@
-function handleMenu(){
-  const body = document.querySelector('body')
-  if(body.classList.contains('open-menu')){
-    body.classList.add('close-menu')
-    body.classList.remove('open-menu')
+function handleMenu() {
+  const body = document.querySelector("body");
+  if (body.classList.contains("open-menu")) {
+    body.classList.add("close-menu");
+    body.classList.remove("open-menu");
   } else {
-    body.classList.add('open-menu')
-    body.classList.remove('close-menu')
+    body.classList.add("open-menu");
+    body.classList.remove("close-menu");
   }
 }
 
-function initPage(){
-  const buttonMenu = document.getElementById('buttonMenu');
-  buttonMenu.addEventListener('click', handleMenu)
+function scrollSmooth() {
+  const enlaces = document.querySelectorAll(".nav a");
 
-  const year = document.getElementById('year');
-  year.textContent = new Date().getFullYear()
+  enlaces.forEach(function (enlace) {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+      const seccion = document.querySelector(e.target.attributes.href.value);
+      const body = document.querySelector("body");
+
+      if (body.classList.contains("open-menu")) {
+        body.classList.add("close-menu");
+        body.classList.remove("open-menu");
+      }
+      seccion.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
 }
 
-window.addEventListener('load', initPage)
+function initPage() {
+  const buttonMenu = document.getElementById("buttonMenu");
+  buttonMenu.addEventListener("click", handleMenu);
+
+  scrollSmooth();
+
+  const year = document.getElementById("year");
+  year.textContent = new Date().getFullYear();
+}
+
+window.addEventListener("load", initPage);
