070d63b21c0feb0fe3084f329d145455f0c2d3eb
Ernesto Rivera <dev@lernesto.net>
2025-08-03 23:42:59 -0600

    Compilacion de pagina estatica

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig,passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: vercel(),
+  output: "static",
+
   image: {
     service: passthroughImageService(),
   }
