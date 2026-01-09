0318bb019a5b70094ec424d813800a36392fd5ff
Ernesto Rivera <dev@lernesto.net>
2026-01-09 08:26:36 -0600

    delete line code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig,passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
